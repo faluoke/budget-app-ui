1975a50e9984a5ab04d93ce36e6389b7c1ad5daa
Faluoke Aisikaer <15935486+faluoke@users.noreply.github.com>
2020-06-01 14:55:11 -0600

    fixed issue with transactions tab loading on mobile

--- src/pages/MobileTransactions.js
+++ src/pages/MobileTransactions.js
@@ -1,10 +1,43 @@
-import React, { useContext } from "react";
+import React, { useState, useEffect } from "react";
 import MobileNav from "../components/MobileNav";
-import { BudgetContext } from "../components/Context/BudgetContext";
+import LoadingSpinner from "../components/LoadingSpinner";
+import axios from "axios";
 
-function MobileTransactions(props) {
-  const [budget] = useContext(BudgetContext);
-  return (
+function MobileTransactions() {
+  const [transactions, setTransactions] = useState({
+    loading: false,
+    data: [],
+  });
+  const fetchTransactions = () => {
+    setTransactions({ loading: true });
+    axios
+      .get("https://master-budget-app.herokuapp.com/api/transactions", {
+        headers: {
+          "auth-token": localStorage.getItem("userInfo"),
+          "user-id": sessionStorage.getItem("email"),
+        },
+      })
+      .then((response) => {
+        if (response.data) {
+          let transactionsClone = response.data.slice();
+          transactionsClone = response.data;
+          setTransactions({ loading: false, data: transactionsClone });
+          console.log(transactions);
+        }
+      })
+      .catch((err) => {
+        setTransactions({ loading: false });
+        console.log(err);
+      });
+  };
+
+  useEffect(() => {
+    fetchTransactions();
+  }, []);
+
+  return transactions.loading ? (
+    <LoadingSpinner />
+  ) : (
     <div className="card transactions">
       <header className="card-header">
         <h1 className="card-header-title">Transactions</h1>
@@ -12,10 +45,10 @@ function MobileTransactions(props) {
       <div className="card-content">
         <div className="content is-medium">
           <ul>
-            {budget.map((budgetItem) => {
+            {transactions.data.map((transaction) => {
               return (
-                <li key={budgetItem._id}>
-                  {budgetItem.type}: {budgetItem.name} - {budgetItem.amount}
+                <li key={transaction._id}>
+                  {transaction.name} - {transaction.amount} ({transaction.type})
                 </li>
               );
             })}
